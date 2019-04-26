20e89ffe1de5dcea22274cac122fb162df121eca
Samoeun <samoeunveth99@gmail.com>
2019-04-26 14:44:17 +0700

    new update

--- src/controller/Connection.java
+++ src/controller/Connection.java
@@ -3,5 +3,6 @@ package controller;
 public class Connection {
     public static void main(String[] args) {
         System.out.println("This is connection class.");
+        System.out.println("This is connection class.");
     }
 }
