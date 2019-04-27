f1a7e586e0f3df261e31e4c69b1b0d1d60e75faf
Sokha <yornsokha168@gmail.com>
2019-04-27 17:09:44 +0700

    add all features of me

--- src/main/Run.java
+++ src/main/Run.java
@@ -9,20 +9,101 @@ import java.util.*;
 
 public class Run {
     private static HashMap<Integer, Product> products;
+    private static int numOfRows = 5;
+    private static int currentPage = 1;
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        generateData();
+//        generateData();
         getData();
-        writeData();
-        readData();
-        saveUpdate();
+//        writeData();
+//        readData();
+//        saveUpdate();
+        System.out.println("=================GO FIRST===================");
+        goFirst();
+        System.out.println("================== GO TO PAGE 4==================");
+        gotoPage(4);
+        System.out.println("================== GO TO LAST PAGE==================");
+        goLast();
+        System.out.println("=================== GO PREVIOUS=================");
+        setRow();
+        System.out.println("================== GO First==================");
+        goFirst();
+        System.out.println("================== GO TO PAGE 10==================");
+        gotoPage(10);
+        System.out.println("=================GO PREVIOUS===================");
+        goPrevious();
+        System.out.println("==================GO PREVIOUS==================");
+        goPrevious();
+        System.out.println("===================GO PREVIOUS=================");
+        goPrevious();
+        System.out.println("===================GO Next=================");
+        goNext();
+        System.out.println("===================GO FIRST=================");
+        goFirst();
+
+
+    }
+
+    private static void setRow() {
+        System.out.print("Number of row : ");
+        scanner.next();
+        numOfRows = scanner.nextInt();
+    }
+
+    private static void goNext() {
+        if(currentPage != getTotalPage())
+            gotoPage(++currentPage);
+    }
+
+    private static void goPrevious(){
+        if(currentPage != 1)
+        gotoPage(--currentPage);
+    }
+
+    private static void gotoPage(int pageNum) {
+        currentPage = pageNum;
+        int start = numOfRows * (currentPage - 1) + 1;
+        for (int i = start; i < start + numOfRows; i++) {
+            System.out.println(products.get(i).toString());
+        }
+        printPageSummary();
+    }
+
+    private static int getTotalPage() {
+        return products.size() % numOfRows == 0 ? products.size() / numOfRows : products.size() / numOfRows + 1;
+    }
+
+    private static int getCurrentPage() {
+        return currentPage;
+    }
+
+    private static void goFirst() {
+        currentPage = 1;
+        for (int i = 1; i <= numOfRows; i++) {
+            System.out.println(products.get(i).toString());
+        }
+        printPageSummary();
+    }
+
+    private static void printPageSummary() {
+        System.out.println("Page : " + currentPage + " of " + getTotalPage() + "\t\t\tTotal record : " + products.size());
+    }
+
+    private static void goLast() {
+        currentPage = getTotalPage();
+        for (int i = products.size() - (numOfRows - 1); i <= products.size(); i++) {
+            System.out.println(products.get(i).toString());
+        }
+
+        printPageSummary();
     }
 
     private static void readData() {
         System.out.print("Read by ID : ");
         int id = scanner.nextInt();
         Product product = products.get(id);
-        if(product != null){
+        if (product != null) {
             System.out.println(product.toString());
         }
     }
@@ -52,7 +133,7 @@ public class Run {
         System.out.println("Read using " + (double) time / 1000000 + " milliseconds");
     }
 
-    private static String getDate(){
+    private static String getDate() {
         DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
         Date date = new Date();
 //        System.out.println(dateFormat.format(date)); //2016/11/16 12:08:43
@@ -78,8 +159,8 @@ public class Run {
         char answer;
         System.out.print("Are you sure to add record? [Y/y] or [N/n]:");
         answer = Character.toLowerCase(scanner.next().charAt(0));
-        if(answer == 'y')
-            products.put(lastId + 1, new Product(lastId+1, name, price, qty, getDate()));
+        if (answer == 'y')
+            products.put(lastId + 1, new Product(lastId + 1, name, price, qty, getDate()));
         scanner.nextLine();
 
     }
