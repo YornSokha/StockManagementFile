043070166e74b963d2944cb5f1d44ecd177eac15
Sokha <yornsokha168@gmail.com>
2019-04-27 08:25:15 +0700

    add synchronize block

--- src/main/App.java
+++ src/main/App.java
@@ -7,35 +7,38 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         Thread thread = new Thread(new Fetch());
         thread.start();
+        thread.join();
     }
 }
 
-class Fetch implements Runnable{
+class Fetch implements Runnable {
 
     @Override
-    public void run(){
+    public synchronized void run() {
         long startTime = System.nanoTime();
         FileWriter fileWriter = null;
         try {
-            fileWriter = new FileWriter("File.txt");
+            fileWriter = new FileWriter("Product.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         Product product;
-        for(long i = 1; i < 10000000; i++){
-            product = new Product(i, "Coca", 10d, 1000, "12/12/2019" );
+
+        for (long i = 1; i < 4000; i++) {
+            product = new Product(i, "Coca", 10d, 1000, "12/12/2019");
             try {
                 bufferedWriter.write(product.toString());
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
         long time = System.nanoTime() - startTime;
-        System.out.println("Read using " + (double)time/1000000 + " milliseconds");
+        System.out.println("Read using " + (double) time / 1000000 + " milliseconds");
     }
 }
 
