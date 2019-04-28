f8eb9c7228c094ba65764656fc2050ab614a9706
NimSreytouch <nimsreytouch@gmail.com>
2019-04-29 01:02:27 +0700

    change one

--- src/main/App.java
+++ src/main/App.java
@@ -3,10 +3,5 @@ package main;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("In Sreytoch branch");
-        System.out.println("sreytouch");
-        System.out.println("Hello world");
-        System.out.println("welcome");
-        System.out.println("jhkfshjs");
     }
 }
