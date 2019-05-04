642a620bcad29d62e55618837346a6be9792967b
sok menghok <sok.menghok1@gmail.com>
2019-05-04 22:15:42 +0700

    database open close  connection class

---
+++ src/main/GetConnection.java
@@ -0,0 +1,56 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * * line 72 change part of file
+ * * line 77 config url and database for using
+ * * line 86 set user and password
+ * search this //<<<< to find what to update
+ * GetConnection.connection
+ * GetConnection.openConnection () => open connection
+ * GetConnection.closeConnection () => close connection
+ * */
+
+
+public class GetConnection {
+
+    public static Connection connection = null;
+
+    public static void  openConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");//<<<<update driver
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //<<<< port
+        String url = "jdbc:mysql://localhost:3306/shop";
+
+        try {
+
+            ///<<<< update package connection "user name password "
+            ///<<<< user and password
+            connection = DriverManager.getConnection(url,"root", "root");
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static void closeConnection(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}//endofcloseConnection
+
